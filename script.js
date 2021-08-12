f8af1bea8ac2eb3aac1d9475e2eed000b9d70674
Yukesh31 <vyukeshkumaran@gmail.com>
2021-08-12 15:08:04 +0530

    images changed

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let imageSrc=['https://source.unsplash.com/wgq4eit198Q/900x350','https://source.unsplash.com/WLUHO9A_xik/900x350','https://source.unsplash.com/4yta6mU66dE/900x350','https://source.unsplash.com/900x350?water','https://source.unsplash.com/900x350?nature']
+let imageSrc=['https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Froyal-enfield&psig=AOvVaw1AWisyj_Q-5AkbRnCvgc6a&ust=1628847197861000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDR0_mWq_ICFQAAAAAdAAAAABAJ',
+'https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fpin%2F783978247614732277%2F&psig=AOvVaw1AWisyj_Q-5AkbRnCvgc6a&ust=1628847197861000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDR0_mWq_ICFQAAAAAdAAAAABAP',
+'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wallpapertip.com%2Fwpic%2FTTTJih_royal-enfield-photos-download%2F&psig=AOvVaw1AWisyj_Q-5AkbRnCvgc6a&ust=1628847197861000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDR0_mWq_ICFQAAAAAdAAAAABAa',
+'https://www.google.com/url?sa=i&url=http%3A%2F%2Fm.gettywallpapers.com%2Froyal-enfield-bullet-wallpapers%2F&psig=AOvVaw1AWisyj_Q-5AkbRnCvgc6a&ust=1628847197861000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDR0_mWq_ICFQAAAAAdAAAAABAf',
+'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpinposters.com%2Fautomobiles-wallpaper%2Froyal-enfield-hd-wallpaper%2F&psig=AOvVaw1AWisyj_Q-5AkbRnCvgc6a&ust=1628847197861000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDR0_mWq_ICFQAAAAAdAAAAABAx']
 let i=0;
 let imageTurn
 turn=()=>
@@ -14,13 +18,13 @@ turn=()=>
         i=0;
     }
    }
- imageTurn=setInterval(turn,2000);
+ imageTurn=setInterval(turn,2000*2);
  function pauseTurn() {
    clearInterval(imageTurn);
  }
  function resumeTurn()
  {
-    imageTurn=setInterval(turn,2000);
+    imageTurn=setInterval(turn,2000*2);
  }
  function previousTurn(){
     
@@ -58,3 +62,10 @@ turn=()=>
      x=document.getElementById('image');
      x.className="fade"
  }
+
+
+
+
+
+
+ 
